eecc1beb72262db838e3804d703295747001f65f
Your Name <paologugliel@gmail.com>
2025-04-08 13:51:24 +0200

    errors correction.

--- Python full course/tkinter_module/drag_&_drop.py
+++ Python full course/tkinter_module/drag_&_drop.py
@@ -2,20 +2,24 @@ from tkinter import *
 
 
 def drag_start(event) -> None:
+    """ Get the coordinates of where I clicked in the widget """
+
     # get the widget of the event I'm dealing with
     widget = event.widget
-    # create variables for its position
+    # create attributes for its coordinates
     widget.startX = event.x
     widget.startY = event.y
 
 
 def drag_motion(event) -> None:
+    """ Move the widget """
+
     # get the widget of the event I'm dealing with
     widget = event.widget
-    # get and set the new position
-    x = widget.winfo_x() - widget.startX + event.x
-    y = widget.winfo_y() - widget.startY + event.y
-    widget.place(x=x, y=y)
+    # get and set its new position
+    x_pos = widget.winfo_x() - widget.startX + event.x
+    y_pos = widget.winfo_y() - widget.startY + event.y
+    widget.place(x=x_pos, y=y_pos)
 
 
 window = Tk()
